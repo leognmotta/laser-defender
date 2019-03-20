8a50c5ff86cf1577c6c3dc0315ab23ff43015d29
Leonardo <leo@domboscoatacado.com.br>
2019-03-20 08:04:10 -0300

    Player move

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,17 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
+
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 0.5f;
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        SetUpMoveBoundaries();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        Move();
+    }
+
+    private void Move() {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        var newXPosition = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPosition = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+
+        transform.position = new Vector2(newXPosition, newYPosition);
+    }
+
+    private void SetUpMoveBoundaries() {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 }
